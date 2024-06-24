e0609337a4c52f5bf9c442aafbe3e46a61417323
MatejPetuchovsky <matej.petuchovsky@soitron.com>
2024-06-24 16:13:55 +0200

    Change dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,8 @@
+use console::Term;
+use tui::{backend::CrosstermBackend, Terminal};
+
 fn main() {
+    let terminal = Term::stdout();
+    let backend = CrosstermBackend::new(terminal);
+    let mut terminal = Terminal::new(backend);
 }
